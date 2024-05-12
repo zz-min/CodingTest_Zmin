52aaffa311976aafadfbd3bc3c6662e010900ce8
Ji-min Kim <71712065+zz-min@users.noreply.github.com>
2024-05-12 15:50:10 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 135.06 ms, Memory: 27.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,19 @@
-def solution(arr):
-    answer = [arr[0]]
+# def solution(arr):
+#     answer = [arr[0]]
     
-    for i in range(len(arr)-1):
-        if arr[i] != arr[i+1]:
-            answer.append(arr[i+1])
+#     for i in range(len(arr)-1):
+#         if arr[i] != arr[i+1]:
+#             answer.append(arr[i+1])
+        
+#     return answer
+
+def solution(arr):
+    answer = []
+    
+    for i in arr:
+        if answer[-1:] == [i]: 
+            continue
+        answer.append(i)
         
     return answer
+
